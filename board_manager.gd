03f5848eff8f0118f751cb796f33e3acf625d13c
rockcen <58832128+rockcen9@users.noreply.github.com>
2025-09-15 10:36:27 +0800

    ,

--- board_manager.gd
+++ board_manager.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-const BOARD_SIZE := 16
+const BOARD_SIZE := 8
 var board_tiles: Array[Array] = []
 var tile_scene: PackedScene = preload("res://scenes/BoardTile.tscn")
 
 func _ready() -> void:
 	init_board()
+	center_board()
 
 func init_board():
 	for x in BOARD_SIZE:
@@ -15,3 +16,9 @@ func init_board():
 			tile.setup(x, y, (x + y) % 2 == 0)
 			add_child(tile)
 			board_tiles[x].append(tile)
+
+func center_board() -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	var board_pixel_size = BOARD_SIZE * 64  # 64 pixels per tile
+	var center_offset = (screen_size - Vector2(board_pixel_size, board_pixel_size)) / 2
+	position = center_offset
